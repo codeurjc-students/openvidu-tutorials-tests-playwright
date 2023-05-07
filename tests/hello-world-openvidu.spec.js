7f9e0261f14d6ca93d1a8db43d19aa1327c47c2e
Ismail Jniah <36990177+IsmailJniah@users.noreply.github.com>
2023-05-07 19:48:14 +0200

    Update hello-world-openvidu.spec.js

--- tests/hello-world-openvidu.spec.js
+++ tests/hello-world-openvidu.spec.js
@@ -1,24 +1,31 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect, chromium } = require('@playwright/test');
 
-test('homepage has title and links to intro page', async ({ page }) => {
-  await page.goto('http://127.0.0.1:8080');
+test('homepage has title and links to intro page', async () => {
+  const browser = await chromium.launch({ headless: true , deviceScaleFactor: 1, // especificar el factor de escala de la página
+  userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36', // especificar el user agent
+  args : ["--use-fake-ui-for-media-stream", "--use-fake-device-for-media-stream"]
+});
+
+  const context = await browser.newContext({
+    permissions: ['camera', 'microphone'],
+  });
   
-  // Makin a screenshot of full page
-  await page.screenshot({ path: '../tests/screenshot.jpg', fullPage: true });
-
-  // Expect a title "to contain" a substring.
- // await expect(page).toHaveTitle("openvidu-hello-world");
-
-  // create a locator
-  //const getStarted = page.getByRole('link', { name: 'Join' });
-
-  // Expect an attribute "to be strictly equal" to the value.
-  //await expect(getStarted).toHaveAttribute('href', '/docs/intro');
-
-  // Click the get started link.
-  //await getStarted.click();
+  const page1 = await context.newPage();
+  await page1.goto('http://127.0.0.1:8080');
   
-  // Expects the URL to contain intro.
-  //await expect(page).toHaveURL(/.*intro/);
+  await page1.click('#join input[type="submit"]');
+  await page1.waitForSelector('#session', { visible: true });
+ 
+  const page2 = await context.newPage();
+
+  await page2.goto('http://127.0.0.1:8080');
+  //await page2.fill('#sessionId', 'SessionS');
+  await page2.click('#join input[type="submit"]');
+  await page2.waitForSelector('#session', { visible: true });
+  await page1.waitForTimeout(20000); 
+
+  await page2.screenshot({ path: 'pr.png' });
+  // Cerrar las páginas y el navegador
+  await Promise.all([page1.close(), page2.close()]);
+  await browser.close();  
 });
