fd2a65b236575b9f632c225014f104beb8a4cdce
Ismail Jniah <36990177+IsmailJniah@users.noreply.github.com>
2023-05-09 21:16:31 +0200

    Update hello-world-openvidu.spec.js

--- tests/hello-world-openvidu.spec.js
+++ tests/hello-world-openvidu.spec.js
@@ -27,16 +27,18 @@ const browser = await chromium.launch({ headless: true , deviceScaleFactor: 1, /
    await page2.screenshot({ path: 'pr.png' });
    
    // Buscar los elementos HTML que contienen los streams de video
-   const videoElements = await page2.$$('video');
+   const videoElementsPage1 = await page1.$$('video');
+
+  // Comprobar que hay exactamente dos elementos encontrados
+   expect(videoElementsPage1.length).toEqual(2);
+   
+   // Buscar los elementos HTML que contienen los streams de video
+   const videoElementsPage2 = await page2.$$('video');
 
 // Comprobar que hay exactamente dos elementos encontrados
-   expect(videoElements.length).toEqual(2);
+   expect(videoElementsPage2.length).toEqual(2);
 
-     // Buscar los elementos HTML que contienen los streams de video
-   videoElements = await page1.$$('video');
 
-// Comprobar que hay exactamente dos elementos encontrados
-   expect(videoElements.length).toEqual(2);
   
 
 
