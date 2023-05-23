0fbd316c34b10bea4c89afdaa1dab2313ee0362b
Ismail Jniah <36990177+IsmailJniah@users.noreply.github.com>
2023-05-23 20:48:11 +0200

    Update jest.config.js

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   reporters: [
     'default',
     [HtmlReporter, {
-      outputPath: './test-report/index.html', // Ruta donde se guardará el informe
+      outputPath: './tests/index.html', // Ruta donde se guardará el informe
       pageTitle: 'Reporte de Pruebas', // Título del informe
       includeFailureMsg: true, // Incluir mensajes de error en el informe
       includeConsoleLog: true, // Incluir registros de consola en el informe
