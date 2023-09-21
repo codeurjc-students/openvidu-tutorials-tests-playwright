86c2c8220bc56ae4479ea0f3669001b8c8e6e107
Ismail Jniah <36990177+IsmailJniah@users.noreply.github.com>
2023-09-21 18:53:32 +0200

    Update openvidu-react-web-tutorial.spec.js

--- tests/openvidu-react-web-tutorial.spec.js
+++ tests/openvidu-react-web-tutorial.spec.js
@@ -14,31 +14,29 @@ const browser = await chromium.launch({ headless: true , deviceScaleFactor: 1,
    });
 
    const page1 = await context.newPage();
-   await page1.goto('http://localhost:3000');
+   await page1.goto('http://127.0.0.1:3000');
 
    await page1.click('#join input[type="submit"]');
    await page1.waitForSelector('#session', { visible: true });
-   await page1.waitForTimeout(5000);
-   //const page2 = await context.newPage();
 
-   //await page2.goto('http://localhost:3000');
+   const page2 = await context.newPage();
+
+   await page2.goto('http://127.0.0.1:3000');
    //await page2.fill('#sessionId', 'SessionS');
-   //await page2.click('#join input[type="submit"]');
-   //await page2.waitForSelector('#session', { visible: true });
-   //await page2.waitForTimeout(5000); 
+   await page2.click('#join input[type="submit"]');
+   await page2.waitForSelector('#session', { visible: true });
+   await page2.waitForTimeout(5000); 
    
-   //await page2.screenshot({ path: 'page2.png' });
-   
-   await page1.screenshot({ path: 'page1.png' });
+   await page2.screenshot({ path: 'pr.png' });
    
    // Buscar los elementos HTML que contienen los streams de video
-   const videoElements = await page1.$$('video');
+   const videoElements = await page2.$$('video');
 
    // Comprobar que hay exactamente dos elementos encontrados
    expect(videoElements.length).toEqual(3);
 
    // Cerrar las páginas y el navegador.
 
-  await Promise.all([page1.close()]);
+  await Promise.all([page1.close(), page2.close()]);
   await browser.close();
 });
