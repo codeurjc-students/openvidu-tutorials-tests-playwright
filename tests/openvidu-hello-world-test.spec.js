537266d67d1376e54ca1de63eeab6d7caf8d8416
Ismail Jniah <36990177+IsmailJniah@users.noreply.github.com>
2023-09-26 17:30:28 +0200

    Update openvidu-hello-world-test.spec.js

--- tests/openvidu-hello-world-test.spec.js
+++ tests/openvidu-hello-world-test.spec.js
@@ -1,8 +1,11 @@
+//Test for Angular, React & Vue tutorials.
 const { test, expect, chromium } = require('@playwright/test');
 
+
+
 test('Checking for the presence of two active webcams in an OpenVidu session', async () => {
-const browser = await chromium.launch({ headless: true , deviceScaleFactor: 1, // especificar el factor de escala de la página
-   userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36', // especificar el user agent
+const browser = await chromium.launch({ headless: true , deviceScaleFactor: 1, 
+   userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36', 
    args : ["--use-fake-ui-for-media-stream", "--use-fake-device-for-media-stream"]
  });
 
@@ -11,26 +14,27 @@ const browser = await chromium.launch({ headless: true , deviceScaleFactor: 1, /
    });
 
    const page1 = await context.newPage();
-   await page1.goto('http://127.0.0.1:8080');
-
+   await page1.goto('http://localhost:8080');
+   await page1.fill('#userName', 'Page1');
    await page1.click('#join input[type="submit"]');
    await page1.waitForSelector('#session', { visible: true });
 
    const page2 = await context.newPage();
 
-   await page2.goto('http://127.0.0.1:8080');
-   //await page2.fill('#sessionId', 'SessionS');
+   await page2.goto('http://localhost:8080');
+   await page2.fill('#userName', 'Page2');
    await page2.click('#join input[type="submit"]');
    await page2.waitForSelector('#session', { visible: true });
    await page2.waitForTimeout(5000); 
    
-   await page2.screenshot({ path: 'pr.png' });
+   await page1.screenshot({ path: 'page1.png' });
+   await page2.screenshot({ path: 'page2.png' });
    
    // Buscar los elementos HTML que contienen los streams de video
    const videoElements = await page2.$$('video');
 
    // Comprobar que hay exactamente dos elementos encontrados
-   expect(videoElements.length).toEqual(2);
+   expect(videoElements.length).toEqual(3);
 
    // Cerrar las páginas y el navegador.
 
