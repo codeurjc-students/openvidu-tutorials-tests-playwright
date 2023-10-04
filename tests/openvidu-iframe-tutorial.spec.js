e837c0aabeadb40ebd63063797c8d7251ae8d456
Ismail Jniah <36990177+IsmailJniah@users.noreply.github.com>
2023-10-04 14:23:22 +0200

    Update openvidu-iframe-tutorial.spec.js

--- tests/openvidu-iframe-tutorial.spec.js
+++ tests/openvidu-iframe-tutorial.spec.js
@@ -2,7 +2,7 @@ const { test, expect, chromium } = require('@playwright/test');
 
 test('Checking for the presence of two active webcams in an OpenVidu session', async () => {
 const browser = await chromium.launch({ headless: true , deviceScaleFactor: 1, // especificar el factor de escala de la página
-   userAgent: 'Chrome/88.0.4324.182', // especificar el user agent
+   userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36', // especificar el user agent
    args : ["--use-fake-ui-for-media-stream", "--use-fake-device-for-media-stream"]
  });
 
@@ -12,32 +12,32 @@ const browser = await chromium.launch({ headless: true , deviceScaleFactor: 1, /
 
    const page1 = await context.newPage();
    await page1.goto('http://localhost:8081/');
-
+   await page1.screenshot({ path: 'page1.png' });
    let iframeHandle = await page1.waitForSelector('iframe.openvidu-iframe');
    let frame = await iframeHandle.contentFrame();
  
    // Haz clic en el botón "JOIN" dentro del iframe
    await frame.click('input[type="submit"]');
+   await frame.waitForSelector('#session', { visible: true });
 
-   //const page2 = await context.newPage();
-   
-   await page1.screenshot({ path: 'pr.png' });
-   //await page2.goto('http://localhost:8081/');
-//   await page2.screenshot({ path: 'pr.png' });
-   //iframeHandle = await page2.waitForSelector('iframe.openvidu-iframe');
-   //frame = await iframeHandle.contentFrame();
+   const page2 = await context.newPage();
+
+   await page2.goto('http://localhost:8081/');
+   iframeHandle = await page2.waitForSelector('iframe.openvidu-iframe');
+   frame = await iframeHandle.contentFrame();
  
    // Haz clic en el botón "JOIN" dentro del iframe
-   //await frame.click('input[type="submit"]');
-   //await frame.waitForTimeout(5000); 
+   await frame.click('input[type="submit"]');
+   await frame.waitForSelector('#session', { visible: true });
+   await frame.waitForTimeout(5000); 
    
-   //await page2.screenshot({ path: 'pr.png' });
+   await page2.screenshot({ path: 'pr.png' });
    
    // Buscar los elementos HTML que contienen los streams de video
-   //const videoElements = await frame.$$('video');
+   const videoElements = await frame.$$('video');
 
    // Comprobar que hay exactamente dos elementos encontrados
-   //expect(videoElements.length).toEqual(2);
+   expect(videoElements.length).toEqual(2);
 
    // Cerrar las páginas y el navegador.
 
