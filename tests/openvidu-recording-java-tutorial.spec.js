6bafb018691bf8bc6b0ea13a9d7b19d58c2e8bb1
Ismail Jniah <36990177+IsmailJniah@users.noreply.github.com>
2023-10-09 13:37:54 +0200

    Update openvidu-recording-java-tutorial.spec.js

--- tests/openvidu-recording-java-tutorial.spec.js
+++ tests/openvidu-recording-java-tutorial.spec.js
@@ -1,12 +1,9 @@
-//Test for Angular, React & Vue tutorials.
 const { test, expect, chromium } = require('@playwright/test');
 
-
-
 test('Checking for the presence of two active webcams in an OpenVidu session', async () => {
-const browser = await chromium.launch({ headless: true , deviceScaleFactor: 1, 
-   userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36', 
-   args : ["--use-fake-ui-for-media-stream", "--use-fake-device-for-media-stream","--ignore-certificate-errors"]
+const browser = await chromium.launch({ headless: false , deviceScaleFactor: 1, // especificar el factor de escala de la página
+   userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36', // especificar el user agent
+   args : ["--use-fake-ui-for-media-stream", "--use-fake-device-for-media-stream", "--ignore-certificate-errors"]
  });
 
    const context = await browser.newContext({
@@ -15,25 +12,26 @@ const browser = await chromium.launch({ headless: true , deviceScaleFactor: 1,
 
    const page1 = await context.newPage();
    await page1.goto('https://localhost:5000');
+
    await page1.click('#join-btn');
    await page1.waitForSelector('#session', { visible: true });
 
    const page2 = await context.newPage();
 
    await page2.goto('https://localhost:5000');
-   await page2.fill('#userName', 'Page2');
+   //await page2.fill('#sessionId', 'SessionS');
    await page2.click('#join-btn');
    await page2.waitForSelector('#session', { visible: true });
+   await page2.click('#buttonStartRecording');
    await page2.waitForTimeout(5000); 
-   
-   await page1.screenshot({ path: 'page1.png' });
-   await page2.screenshot({ path: 'page2.png' });
+   await page2.click('#buttonStopRecording');
+   await page2.screenshot({ path: 'pr.png' });
    
    // Buscar los elementos HTML que contienen los streams de video
    const videoElements = await page2.$$('video');
 
    // Comprobar que hay exactamente dos elementos encontrados
-   expect(videoElements.length).toEqual(3);
+   expect(videoElements.length).toEqual(2);
 
    // Cerrar las páginas y el navegador.
 
