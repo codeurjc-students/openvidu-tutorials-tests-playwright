7af5d545ae7be1aa7563bbbc0f1756b6ae008f44
ismail <ismajniah@gmail.com>
2023-10-10 14:11:18 +0200

    deleted:    tests-examples/demo-todo-app.spec.js 	modified:   tests/openvidu-getaroom-test.spec.js

--- tests/openvidu-getaroom-test.spec.js
+++ tests/openvidu-getaroom-test.spec.js
@@ -1,7 +1,7 @@
 const { test, expect, chromium } = require('@playwright/test');
 
 test('Checking for the presence of two active webcams in an OpenVidu session', async () => {
-const browser = await chromium.launch({ headless: true , deviceScaleFactor: 1, // especificar el factor de escala de la página
+const browser = await chromium.launch({ headless: false , deviceScaleFactor: 1, // especificar el factor de escala de la página
    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36', // especificar el user agent
    args : ["--use-fake-ui-for-media-stream", "--use-fake-device-for-media-stream"]
  });
@@ -13,14 +13,18 @@ const browser = await chromium.launch({ headless: true , deviceScaleFactor: 1, /
    const page1 = await context.newPage();
    await page1.goto('http://127.0.0.1:8080');
 
-   await page1.click('#go input[type="submit"]');
+   // Espera a que el botón esté presente en la página
+   await page1.waitForSelector('button.btn-success');
+
+   // Hace clic en el botón con el atributo onclick
+   await page1.click('button.btn-success');
    await page1.waitForSelector('#session', { visible: true });
+   const inputValue = await page1.$eval('input#copy-input', (input) => input.value);
 
    const page2 = await context.newPage();
 
-   await page2.goto('http://127.0.0.1:8080');
-   //await page2.fill('#sessionId', 'SessionS');
-   await page2.click('#go input[type="submit"]');
+   await page2.goto(inputValue);
+
    await page2.waitForSelector('#session', { visible: true });
    await page2.waitForTimeout(5000); 
    
