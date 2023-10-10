e1f2606b398d724442c84085d13a51049b6267e8
ismail <ismajniah@gmail.com>
2023-10-10 18:02:30 +0200

    modified:   tests/openvidu-getaroom-test.spec.js

--- tests/openvidu-getaroom-test.spec.js
+++ tests/openvidu-getaroom-test.spec.js
@@ -1,53 +1,43 @@
 const { test, expect, chromium } = require('@playwright/test');
 
-test('Checking for the presence of two active webcams in an OpenVidu session', async ({ page }) => {
-  const browser = await chromium.launch({
-    headless: true,
-    deviceScaleFactor: 1,
-    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
-    args: ["--use-fake-ui-for-media-stream", "--use-fake-device-for-media-stream"]
-  });
+test('Checking for the presence of two active webcams in an OpenVidu session', async () => {
+const browser = await chromium.launch({ headless: true , deviceScaleFactor: 1, // especificar el factor de escala de la página
+   userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36', // especificar el user agent
+   args : ["--use-fake-ui-for-media-stream", "--use-fake-device-for-media-stream"]
+ });
 
-  const context = await browser.newContext({
-    permissions: ['camera', 'microphone'],
-  });
-  try {
-  const page1 = await context.newPage();
-  await page1.goto('http://127.0.0.1:8080');
+   const context = await browser.newContext({
+     permissions: ['camera', 'microphone'],
+   });
 
+   const page1 = await context.newPage();
+   await page1.goto('http://127.0.0.1:8080');
 
-    // Espera a que el botón esté presente en la página
-    await page1.waitForSelector('button.btn-success', { timeout: 10000 }); // Aumentamos el tiempo límite a 10 segundos
+   // Espera a que el botón esté presente en la página
+   await page1.waitForSelector('button.btn-success');
 
-    // Hace clic en el botón con el atributo onclick
-    await page1.click('button.btn-success');
-    await page1.waitForSelector('#session', { visible: true });
-    const inputValue = await page1.$eval('input#copy-input', (input) => input.value);
-    await page1.screenshot({ path: 'results/screenshots/page1_screenshot.png' });
-    
-    const page2 = await context.newPage();
+   // Hace clic en el botón con el atributo onclick
+   await page1.click('button.btn-success');
+   await page1.waitForSelector('#session', { visible: true });
+   const inputValue = await page1.$eval('input#copy-input', (input) => input.value);
 
-    await page2.goto(inputValue);
+   const page2 = await context.newPage();
 
-    await page2.waitForSelector('#session', { visible: true });
-    await page2.waitForTimeout(5000);
-    await page2.screenshot({ path: 'results/screenshots/page2_screenshot.png' });
-    // Buscar los elementos HTML que contienen los streams de video
-    const videoElements = await page2.$$('video');
+   await page2.goto(inputValue);
 
-    // Comprobar que hay exactamente dos elementos encontrados
-    expect(videoElements.length).toEqual(2);
-    
-  } catch (error) {
+   await page2.waitForSelector('#session', { visible: true });
+   await page2.waitForTimeout(5000); 
    
-  if (error instanceof playwright.errors.TimeoutError) {
-    // Do something if this is a timeout.
-    await page1.screenshot({ path: 'results/screenshots/error_page1_screenshot.png' });
-    await page2.screenshot({ path: 'results/screenshots/error_page2_screenshot.png' });
-  }
-    throw error; // Lanzar el error nuevamente para que la prueba falle
-  } finally {
-    // Cerrar las páginas y el navegador.
-    await Promise.all([page1.close(), browser.close()]);
-  }
+   await page2.screenshot({ path: 'pr.png' });
+   
+   // Buscar los elementos HTML que contienen los streams de video
+   const videoElements = await page2.$$('video');
+
+   // Comprobar que hay exactamente dos elementos encontrados
+   expect(videoElements.length).toEqual(2);
+
+   // Cerrar las páginas y el navegador.
+
+  await Promise.all([page1.close(), page2.close()]);
+  await browser.close();
 });
