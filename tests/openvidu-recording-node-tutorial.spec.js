59aaf2f6b7eeaf6596dfc128019fa09405a06c89
Ismail Jniah <36990177+IsmailJniah@users.noreply.github.com>
2023-10-24 12:46:05 +0200

    Update openvidu-recording-node-tutorial.spec.js

--- tests/openvidu-recording-node-tutorial.spec.js
+++ tests/openvidu-recording-node-tutorial.spec.js
@@ -25,7 +25,7 @@ test('Checking for the presence of two active webcams in an OpenVidu session', a
     await page1.waitForSelector('#session', { visible: true });
     await page1.screenshot({ path: '../results/screenshots/page1_screenshot.png' });
 
-    await page2.goto('https://localhost:5000');
+    await page2.goto('http://localhost:5000');
                       
 
     await page2.click('#join-btn');
