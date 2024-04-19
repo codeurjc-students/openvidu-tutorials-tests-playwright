9813fda3ce7f3026ebd600d1224de8f52e57c8b4
ismail <ismajniah@gmail.com>
2024-04-19 13:12:53 +0200

    changes on Openvidu-vue  test

--- tests/openvidu-vue-tutorial.spec.js
+++ tests/openvidu-vue-tutorial.spec.js
@@ -1,60 +1,61 @@
-//Test for Angular, React & Vue tutorials.
 const { test, expect, chromium } = require('@playwright/test');
 
-
 test('Checking for the presence of two active webcams in an OpenVidu session', async () => {
+  // Inicia una instancia del navegador Chromium en modo headless con configuraciones específicas.
   const browser = await chromium.launch({
-    headless: true, deviceScaleFactor: 1,
+    headless: true,
+    deviceScaleFactor: 1,
     userAgent: 'Chrome/88.0.4324.182',
     args: ["--use-fake-ui-for-media-stream", "--use-fake-device-for-media-stream"]
   });
 
+  // Crea un nuevo contexto en el navegador con los permisos necesarios.
   const context = await browser.newContext({
     permissions: ['camera', 'microphone'],
   });
 
+  // Crea dos nuevas páginas dentro del contexto del navegador.
   const page1 = await context.newPage();
   const page2 = await context.newPage();
 
   try {
+    // Navega a una URL específica en la primera página y realiza acciones como llenar un campo de texto y hacer clic en un botón.
     await page1.goto('http://localhost:8080');
     await page1.fill('input[type="text"]', 'User1');
     await page1.click('button.btn.btn-lg.btn-success');
+    // Espera a que un elemento específico se vuelva visible.
     await page1.waitForSelector('#session', { visible: true });
-    
-    var videoElements = await page1.$$('video');
+    await page1.waitForTimeout(5000);ç
 
+    // Encuentra los elementos de video en la página y verifica que haya exactamente dos de ellos.
+    var videoElements = await page1.$$('video');
     expect(videoElements.length).toEqual(2);
 
+    // Captura una captura de pantalla de la página 1 y la guarda en un archivo.
     await page1.screenshot({ path: '../results/screenshots/page1_screenshot.png' });
     
+    // Navega a una URL específica en la segunda página y realiza acciones similares a las de la primera página.
     await page2.goto('http://localhost:8080');
     await page2.fill('input[type="text"]', 'User2');
     await page2.click('button.btn.btn-lg.btn-success');
     await page2.waitForSelector('#session', { visible: true });
     await page2.waitForTimeout(5000);
   
-    // Buscar los elementos HTML que contienen los streams de video
+    // Busca los elementos de video en la segunda página y verifica que haya exactamente dos de ellos.
     videoElements = await page2.$$('video');
-  
-    // Comprobar que hay exactamente dos elementos encontrados
-    expect(videoElements.length).toEqual(3);
-    
+    expect(videoElements.length).toEqual(2);
 
+    // Captura una captura de pantalla de la página 2 y la guarda en un archivo.
     await page2.screenshot({ path: '../results/screenshots/page2_screenshot.png' });
 
-    // Cerrar las páginas y el navegador.
-  
+    // Cierra las páginas y el navegador.
     await Promise.all([page1.close(), page2.close()]);
     await browser.close();
 
   } catch (error) {
-
-    // En caso de error, captura las pantallas y registra el error
+    // En caso de error, captura las pantallas y registra el error.
     await page1.screenshot({ path: '../results/screenshots/error_page1_screenshot.png' });
     await page2.screenshot({ path: '../results/screenshots/error_page2_screenshot.png' });
-    throw error; // Lanza el error nuevamente para que el test falle
+    throw error; // Lanza el error nuevamente para que la prueba falle.
   }
-
-
 });
